e50785c1d671fc36e91b8ea1bfdf9626927f7c02
Mojakun <kazuya.6289@gmail.com>
2023-04-01 08:57:55 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa")({
+  dest: "public",
+});
+module.exports = withPWA({
   reactStrictMode: true,
-}
-
-module.exports = nextConfig
+});
